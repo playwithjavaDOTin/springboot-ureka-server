a8fc52d8e956091eb2d485b90b29010dabdb455f
aray <aray@altimetrik.com>
2023-07-08 15:59:54 +0530

    initial commit by Admin

---
+++ src/test/java/playwithjava/in/SpringbootUrekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package playwithjava.in;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootUrekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
